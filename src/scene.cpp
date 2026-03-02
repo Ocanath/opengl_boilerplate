078ccfdc1f9a96b2e312bd32ffc4f56f90831810
ocanath <ocanath@gmail.com>
2026-03-02 13:41:18 -0800

    walls

--- src/scene.cpp
+++ src/scene.cpp
@@ -37,7 +37,7 @@ Scene::Scene()
 
     camera_ = std::make_unique<Camera>(dynamicsWorld_);
 
-    buildChamber(glm::vec3{200,400,50});
+    buildChamber(glm::vec3{200,400,100});
     buildPillars();
 
     // Create deferred rendering shaders
