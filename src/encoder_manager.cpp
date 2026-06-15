20391c1a0751d416c0f2435a9fff45d2af8961dd
ocanath <ocanath@gmail.com>
2026-06-14 22:55:17 -0700

    read thread is getting data but the arm ain't moving because vibes

--- src/encoder_manager.cpp
+++ src/encoder_manager.cpp
@@ -1,8 +1,10 @@
 #include "encoder_manager.h"
+#include "encoder.h"
 #include <chrono>
 
 EncoderManager::EncoderManager()
 {
+    thetas_.resize(2, 0.f);   // one slot per encoder
     running_ = true;
     thread_  = std::thread(&EncoderManager::pollLoop, this);
 }
@@ -23,12 +25,30 @@ std::vector<float> EncoderManager::getThetas() const
 void EncoderManager::pollLoop()
 {
     // TODO: set up Serial + Encoders here
-
-    while (running_) {
+	Serial ser;
+	ser.autoconnect(921600);
+	std::vector<Encoder *> enc_arm;
+	Encoder e1(0, &ser);
+	Encoder e2(1, &ser);
+	enc_arm.push_back(&e1);
+	enc_arm.push_back(&e2);
+	
+    while (running_) 
+	{
         // TODO: read encoders, then:
         // std::lock_guard<std::mutex> lk(mutex_);
         // thetas_[i] = encoders[i]->theta;
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+		for(int i = 0; i < enc_arm.size(); i++)
+		{
+			int rc = enc_arm[i]->read_angle();
+			if(rc != DARTT_PROTOCOL_SUCCESS)
+			{
+				printf("Failed to read encoder %d: code %d\n", i, rc);
+			}
+			std::lock_guard<std::mutex> lk (mutex_);
+			thetas_[i] = enc_arm[i]->theta;
+		}
+		printf("[%f, %f]\n", enc_arm[0]->theta, enc_arm[1]->theta);
+        // std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
 }
